0a1734515ed04c82bdd1f3101ff02030ff26c12f
Aditya Babar <babaraditya02@gmail.com>
2025-01-10 21:18:13 +0530

    Changed the logic to decide which scene to load

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -13,10 +13,16 @@ public class GameStartManager : MonoBehaviour
     void Awake()
     {
         // Check if the cutscene has already been watched
-        if (PlayerPrefs.GetInt("CutsceneCompleted", 0) == 1 && PlayerPrefs.GetString("CompletedSceneName").Equals(introSceneNameTwo))
+        // if (PlayerPrefs.GetInt("CutsceneCompleted", 0) == 1 && PlayerPrefs.GetString("CompletedSceneName").Equals(introSceneNameTwo))
+        // {
+        //     // Load the main game scene directly
+        //     SceneManager.LoadScene(gameSceneName);
+        // }
+
+        //Check if play button is clicked
+        if (PlayerPrefs.GetInt("PlayButtonClicked", 0) == 1 )
         {
-            // Load the main game scene directly
-            SceneManager.LoadScene(gameSceneName);
+             SceneManager.LoadScene(gameSceneName);
         }
 
     }
