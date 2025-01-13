a3fc06d74d9d0f41f43ad2840d1089a471cc0173
Aditya Babar <babaraditya02@gmail.com>
2025-01-13 21:20:24 +0530

    Fixed the bug in NewAnimationCutScene_1 which prevented it from load and caused black screen

--- Assets/Scripts/ManagerScripts/GameLaunchManager.cs
+++ Assets/Scripts/ManagerScripts/GameLaunchManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameStartManager : MonoBehaviour
+public class GameLaunchManager : MonoBehaviour
 {
     [SerializeField] string introSceneName; // Scene to load initially
     [SerializeField] string introSceneNameTwo; // Optional second intro scene
@@ -11,16 +11,19 @@ public class GameStartManager : MonoBehaviour
 
     void Awake()
     {
+        Debug.Log(PlayerPrefs.GetInt("PlayButtonClicked", 0));
+        Debug.Log(gameSceneName);
+        Debug.Log(introSceneName);
         // Check if the "Play" button has been clicked previously
         if (PlayerPrefs.GetInt("PlayButtonClicked", 0) == 1) 
         {
             // If clicked, load the main game scene directly
             SceneManager.LoadScene(gameSceneName); 
         }
-        else 
-        {
-            // If not clicked, load the first intro scene
-            SceneManager.LoadScene(introSceneName); 
-        }
+        // else 
+        // {
+        //     // If not clicked, load the first intro scene
+        //     SceneManager.LoadScene(introSceneName); 
+        // }
     }
 }
