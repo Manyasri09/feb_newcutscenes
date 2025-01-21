c19835e5370ae0dbc9fd46f33d91ebd010d091cc
Aditya Babar <babaraditya02@gmail.com>
2025-01-21 19:57:16 +0530

    Added property classes to LevelConfigSO

--- Assets/Custom_Packages/Package_CMSv1/Runtime/Scripts/Scriptable/LevelConfiggSO.cs
+++ Assets/Custom_Packages/Package_CMSv1/Runtime/Scripts/Scriptable/LevelConfiggSO.cs
@@ -5,16 +5,47 @@ using UnityEngine;
 namespace ezygamers.cmsv1
 {
     
-    public class LevelConfiggSO : ScriptableObject
+    public class LevelConfigSO : ScriptableObject
     {
+        // Public field to store the level number
         public int LevelNumber;
-       
+        // Public field to store the number of sublevels
         public int noOfSubLevel;
 
-        //option 
-        public List<QuestionBaseSO> question = new List<QuestionBaseSO>();
-        
+        // Private backing field for questions
+        [SerializeField]
+        private List<QuestionBaseSO> questions = new List<QuestionBaseSO>();
 
+        // Property to access questions
+        public List<QuestionBaseSO> Questions
+        {
+            get => questions;
+            set
+            {
+                if (value != null)
+                {
+                    questions = value;
+                }
+                else
+                {
+                    Debug.LogError("Attempted to set questions to null!");
+                }
+            }
+        }
+
+        // Optionally, a property to get a specific question
+        public QuestionBaseSO this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= questions.Count)
+                {
+                    Debug.LogError($"Invalid question index: {index}");
+                    return null;
+                }
+                return questions[index];
+            }
+        }
     }
 
     
